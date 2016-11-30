1aed4b2358b0ca4b1fc50607b91ae530001adb5a
Tamaki Nishino <otamachan@gmail.com>
2016-12-01 08:07:21 +0900

    Update ros-indigo-gazebo7

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install aptly
 aptly config show
 aptly repo create trusty
-wget https://github.com/otamachan/ros-indigo-gazebo7-deb/releases/download/debian%2F7.4.0-0_trusty1/ros-indigo-gazebo7_7.4.0-0.trusty1_amd64.deb
+wget https://github.com/otamachan/ros-indigo-gazebo7-deb/releases/download/debian%2F7.4.0-1/ros-indigo-gazebo7_7.4.0-1_amd64.deb
 wget https://github.com/otamachan/ros-indigo-sdformat4-deb/releases/download/debian%2F4.2.0-0_trusty1/ros-indigo-sdformat4_4.2.0-0.trusty1_amd64.deb
 aptly repo add trusty *.deb
 aptly publish repo -component="main" -distribution="trusty" -label="ROS indigo" -origin="otamachan" -skip-signing=true trusty
