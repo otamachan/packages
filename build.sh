c4110a86434c01ba39c3a8a9680df09c2ab57c29
Tamaki Nishino <otamachan@gmail.com>
2016-12-02 21:18:19 +0900

    Hide security environmental variables

--- build.sh
+++ build.sh
@@ -10,10 +10,4 @@ wget https://github.com/otamachan/ros-indigo-gazebo7-deb/releases/download/debia
 wget https://github.com/otamachan/ros-indigo-sdformat4-deb/releases/download/debian%2F4.2.0-0_trusty1/ros-indigo-sdformat4_4.2.0-0.trusty1_amd64.deb
 aptly repo add trusty *.deb
 aptly publish repo -component="main" -distribution="trusty" -label="ROS indigo" -origin="otamachan" -skip-signing=true trusty
-cd ${HOME}/.aptly/public
-git init
-git config user.name "Travis CI"
-git config user.email "otamachan@mail.com"
-git add * --all
-git commit -m "Publish repository"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
+
